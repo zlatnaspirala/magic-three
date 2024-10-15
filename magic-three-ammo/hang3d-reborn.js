55892a505821fd035da3c7a0a1721fcfadb7b91f
zlatnaspirala <zlatnaspirala@gmail.com>
2024-10-15 22:06:20 +0200

    wip

--- magic-three-ammo/hang3d-reborn.js
+++ magic-three-ammo/hang3d-reborn.js
@@ -1,12 +1,13 @@
-
+/**
+ * @description Main Appication Instance
+ * @author Nikola Lukic
+ * @email zlatnaspirala@gmail.com
+ */
 import Application from './Application.js';
 import config from './config.js';
 import myGamePlayMagicMap from './public/assets/maps/free-for-all.js';
 
 let App = new Application(config, myGamePlayMagicMap);
 
-// App.activateNet();
-
-// Remove this after all
-// this is only for easy access from console
+// global
 window.App = App;
